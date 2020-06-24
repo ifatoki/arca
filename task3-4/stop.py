dfbeee79e2a1101c1a5946cb16e3f99414d69f72
Itunuloluwa Fatoki <itunuworks@gmail.com>
2020-06-24 09:35:54 +0100

    feature: implement instance start stop script

---
+++ task3-4/stop.py
@@ -0,0 +1,11 @@
+import boto3
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--region', action='store', required=True)
+parser.add_argument('--instance-id', action='store', required=True)
+args = parser.parse_args()
+
+ec2 = boto3.resource('ec2', region_name=args.region)
+instance = ec2.Instance(args.instance_id)
+instance.stop()
