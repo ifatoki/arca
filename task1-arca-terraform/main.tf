ebac5d59c00515b256983b1368564fe8067bbcaf
Itunuloluwa Fatoki <itunuworks@gmail.com>
2020-06-24 19:41:59 +0100

    feature: implement terraform script to deploy ubuntu 18.04 server * configure the server to automatically run the docker scripts

---
+++ task1-arca-terraform/main.tf
@@ -0,0 +1,120 @@
+# Specify the provider and access details
+provider "aws" {
+  region = var.aws_region
+}
+
+# Create a VPC to launch our instances into
+resource "aws_vpc" "default" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_hostnames = true
+}
+
+# Create an internet gateway to give our subnet access to the outside world
+resource "aws_internet_gateway" "default" {
+  vpc_id = aws_vpc.default.id
+}
+
+resource "aws_route" "internet_access" {
+  route_table_id         = aws_vpc.default.main_route_table_id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.default.id
+}
+
+# Create a subnet to launch our instances into
+resource "aws_subnet" "default" {
+  vpc_id                  = aws_vpc.default.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+}
+
+# Our default security group to access
+# the instances over SSH and HTTP
+resource "aws_security_group" "default" {
+  name        = "terraform_example"
+  description = "Used in the terraform"
+  vpc_id      = aws_vpc.default.id
+
+  # SSH access from anywhere
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # HTTP access from the VPC
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # outbound internet access
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_key_pair" "auth" {
+  key_name   = var.key_name
+  public_key = file(var.public_key_path)
+}
+
+resource "aws_instance" "web" {
+  # The connection block tells our provisioner how to
+  # communicate with the resource (instance)
+  connection {
+    # The default username for our AMI
+    user = "ubuntu"
+    host = self.public_ip
+    # The connection will use the local SSH agent for authentication.
+  }
+
+  instance_type = "t2.micro"
+
+  # Lookup the correct AMI based on the region
+  # we specified
+  ami = lookup(var.aws_amis, var.aws_region)
+
+  # The name of our SSH keypair we created above.
+  key_name = var.key_name
+
+  # Our Security group to allow HTTP and SSH access
+  vpc_security_group_ids = [aws_security_group.default.id]
+
+  # We're going to launch into the same subnet as our ELB. In a production
+  # environment it's more common to have a separate private subnet for
+  # backend instances.
+  subnet_id = aws_subnet.default.id
+
+  provisioner "file" {
+    source      = "./scripts/deploy.sh"
+    destination = "/tmp/deploy.sh"
+  }
+
+  # We run a remote provisioner on the instance after creating it.
+  # In this case, we just run the script creating the containers. By default,
+  # nginx should be available on port 80
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt update",
+      "sudo apt --yes install software-properties-common apt-transport-https ca-certificates curl",
+      "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -",
+      "sudo add-apt-repository \"deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable\"",
+      "sudo apt update",
+      "sudo apt --yes install docker-ce",
+      "sudo usermod --append --groups docker $USER"
+    ]
+  }
+  
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /tmp/deploy.sh",
+      "/tmp/deploy.sh"
+    ]
+  }
+}
